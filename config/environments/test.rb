1ee8b96754d7377c86c121c9d90756b8b7a4582e
Mykola Saienko <nicolas.ozz.com@gmail.com>
2021-03-15 18:25:22 +0200

    Add localeapp

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -56,4 +56,6 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  config.i18n.available_locales = ['es-CO', :de]
 end
