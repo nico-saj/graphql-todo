c4751c715bb04771fd19f5faba85df0672a29ea0
Mykola Saienko <nicolas.ozz.com@gmail.com>
2021-03-15 19:14:16 +0200

    Update locale

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,5 +118,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.i18n.available_locales = ['es-CO', :de]
+  config.i18n.available_locales = [:de]
 end
